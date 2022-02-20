79b3ba50416b949e40997cd772766d99dd6831e2
Florian <florianberlin7516@gmail.com>
2022-02-20 10:32:05 +0100

    Added player and camera movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float moveForce = 10f;
+    
+    [SerializeField]
+    private float jumpForce = 11f;
+    
+    private float _movementX;
+
+    private Rigidbody2D _myBody;
+    private SpriteRenderer _sr;
+    private Animator _anim;
+    private string WALK_ANIMATION = "isWalking";
+    
+    private bool isGrounded = true;
+    private string GROUND_TAG = "Ground";
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _myBody = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
+        _sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+
+    }
+
+    private void FixedUpdate()
+    {
+        PlayerJump();
+    }
+
+    void PlayerMoveKeyboard()
+    {
+        this._movementX = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(_movementX, 0f, 0f) * (Time.deltaTime * moveForce);
+    }
+
+    void AnimatePlayer()
+    {
+        if (_movementX > 0)
+        {
+            _anim.SetBool(WALK_ANIMATION, true);
+            _sr.flipX = false;
+        }
+        else if (_movementX < 0)
+        {
+            _anim.SetBool(WALK_ANIMATION, true);
+            _sr.flipX = true;
+        }
+        else
+        {
+            _anim.SetBool(WALK_ANIMATION, false);
+        }
+    }
+
+    void PlayerJump()
+    {
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            isGrounded = false;
+            _myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        if (col.gameObject.CompareTag(GROUND_TAG))
+        {
+            isGrounded = true;
+        }
+    }
+} // class
